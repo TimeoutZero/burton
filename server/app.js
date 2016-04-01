1a32ec260e0629f0e723a9683f1d80af4ea7463d
lucasgmartins <lucas.gmmartins@gmail.com>
2016-04-01 20:57:36 -0300

    start

--- server/app.js
+++ server/app.js
@@ -1,1 +1,9 @@
+/// <reference path="typings/tsd.d.ts" />
+
+
 var express = require('express');
+var burton = express();
+ 
+burton.listen(9000, () => {
+    console.log('[BURTON] Started...');
+});
